d492488d8b57fcebc3dcbfd0c4b9f82809984e15
Boris van Katwijk <bvkatwijk@gmail.com>
2024-01-28 12:21:25 +0100

    Renames

--- example/src/test/java/org/bvkatwijk/lens/example/ExampleTest.java
+++ example/src/test/java/org/bvkatwijk/lens/example/ExampleTest.java
@@ -4,6 +4,7 @@ import lombok.With;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,30 +18,38 @@ public class ExampleTest {
     private static final String NAME_NEW = "Bob";
 
     @With
-    public record Address(String street, int number) { }
-
-    @With
-    public record User(String name, Address address) {
-        public <TARGET> X<TARGET> focus(Function<User, TARGET> f, Function<TARGET, User> with) {
-            return new X<>(this, with, f);
+    public record Address(String street, int number) {
+        public <TARGET> Lens<Address, TARGET> focus(Function<Address, TARGET> f, Function<TARGET, Address> with) {
+            return new Lens<>(this, with, f);
         }
     }
 
-    public record X<TARGET>(User user, Function<TARGET, User> withX, Function<User, TARGET> f) {
-        public <NEW_TARGET> X<NEW_TARGET> focus(
-            Function<NEW_TARGET, TARGET> with,
-            Function<TARGET, NEW_TARGET> number
+    @With
+    public record User(String name, Address address) {
+        public <TARGET> Lens<User, TARGET> focus(Function<User, TARGET> f, Function<TARGET, User> with) {
+            return new Lens<>(this, with, f);
+        }
+    }
+
+    public record Lens<SOURCE, TARGET>(SOURCE source, Function<TARGET, SOURCE> with, Function<SOURCE, TARGET> zoom) {
+        public <NEW_TARGET> Lens<SOURCE, NEW_TARGET> focus(
+            Function<NEW_TARGET, TARGET> nextWith,
+            Function<TARGET, NEW_TARGET> nextZoom
         ) {
-            return new X<>(user, withX.compose(with), f.andThen(number));
+            return new Lens<>(source, with.compose(nextWith), zoom.andThen(nextZoom));
         }
 
-        public User replace(TARGET i) {
-            return withX.apply(i);
+        public SOURCE replace(TARGET i) {
+            return with.apply(i);
+        }
+
+        public SOURCE modify(UnaryOperator<TARGET> f) {
+            return with.apply(this.zoom.andThen(f).apply(this.source));
         }
     }
 
     @Test
-    void lens() {
+    void userAddressNumberReplace() {
         var addressLens = USER.focus(User::address, USER::withAddress);
         assertEquals(
             new User(NAME, NEW_ADDRESS),
@@ -51,11 +60,29 @@ public class ExampleTest {
     }
 
     @Test
-    void lensName() {
+    void userAddressNameReplace() {
         assertEquals(
             new User(NAME_NEW, ADDRESS),
             USER.focus(User::name, USER::withName)
                 .replace(NAME_NEW)
+        );
+    }
+
+    @Test
+    void addressNumberReplace() {
+        assertEquals(
+            NEW_ADDRESS,
+            ADDRESS.focus(Address::number, ADDRESS::withNumber)
+                .replace(NUMBER_NEW)
+        );
+    }
+
+    @Test
+    void addressNumberModify() {
+        assertEquals(
+            NEW_ADDRESS,
+            ADDRESS.focus(Address::number, ADDRESS::withNumber)
+                .modify(i -> i + 1)
         );
     }
 }
