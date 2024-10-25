e73756501f5624613a3569c31a36aa2a4eea3700
Boris van Katwijk <bvkatwijk@gmail.com>
2024-10-26 00:13:24 +0200

    Update LensZ.java

--- lens/src/test/java/nl/bvkatwijk/lens/LensZ.java
+++ lens/src/test/java/nl/bvkatwijk/lens/LensZ.java
@@ -1,12 +1,7 @@
 package nl.bvkatwijk.lens;
 
-import io.vavr.Function1;
-import io.vavr.Function2;
 import nl.bvkatwijk.lens.gen.AddressLens;
-import nl.bvkatwijk.lens.gen.CityLens;
 import nl.bvkatwijk.lens.gen.PersonLens;
-import nl.bvkatwijk.lens.kind.ILens;
-import nl.bvkatwijk.lens.kind.Lens;
 
 public class LensZ {
     public static final PersonZ PERSON = new PersonZ();
@@ -14,22 +9,6 @@ public class LensZ {
     public record PersonZ() {
         public AddressLens<Person> address() {
             return new AddressLens<>(PersonLens.ADDRESS);
-        }
-    }
-
-    public record CityZ<T>(Lens<T, City> inner) implements ILens<T, City> {
-        @Override
-        public Function1<T, City> get() {
-            return inner.get();
-        }
-
-        @Override
-        public Function2<T, City, T> with() {
-            return inner.with();
-        }
-
-        public ILens<T, String> name() {
-            return inner.andThen(CityLens.NAME);
         }
     }
 }
