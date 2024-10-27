05adf065f646aa8b0eddd0cf529f1b536d94d971
Boris van Katwijk <bvkatwijk@gmail.com>
2024-10-27 15:06:34 +0100

    Update Lens.java

--- lens/src/main/java/nl/bvkatwijk/lens/kind/Lens.java
+++ lens/src/main/java/nl/bvkatwijk/lens/kind/Lens.java
@@ -1,9 +1,10 @@
 package nl.bvkatwijk.lens.kind;
 
 import io.vavr.Function1;
-import io.vavr.Function2;
 
-public record Lens<S, T>(Function2<S, T, S> with, Function1<S, T> get) implements ILens<S, T> {
+import java.util.function.BiFunction;
+
+public record Lens<S, T>(BiFunction<S, T, S> with, Function1<S, T> get) implements ILens<S, T> {
     public static <T> Lens<T, T> identity() {
         return new Lens<>((a, b) -> b, Function1.identity());
     }
