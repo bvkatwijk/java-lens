0879afd74403a21e28302532a15f904e6106cce7
Boris van Katwijk <bvkatwijk@gmail.com>
2024-11-03 11:19:25 +0100

    Update Lens.java

--- lens/src/main/java/nl/bvkatwijk/lens/kind/Lens.java
+++ lens/src/main/java/nl/bvkatwijk/lens/kind/Lens.java
@@ -3,6 +3,13 @@ package nl.bvkatwijk.lens.kind;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
+/**
+ * Basic implementation of {@link ILens}
+ * @param get getter function
+ * @param with wither function
+ * @param <S> Source type
+ * @param <T> Target type
+ */
 public record Lens<S, T>(Function<S, T> get, BiFunction<S, T, S> with) implements ILens<S, T> {
     public static <T> Lens<T, T> identity() {
         return new Lens<>(Function.identity(), (a, b) -> b);
