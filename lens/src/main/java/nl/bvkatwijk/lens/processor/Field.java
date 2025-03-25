f0c22440ccf9fb26b55519b2c5af555908297b7a
Boris van Katwijk <bvkatwijk@gmail.com>
2025-03-25 15:13:15 +0100

    Update Field.java

--- lens/src/main/java/nl/bvkatwijk/lens/processor/Field.java
+++ lens/src/main/java/nl/bvkatwijk/lens/processor/Field.java
@@ -1,9 +1,4 @@
 package nl.bvkatwijk.lens.processor;
 
-import javax.lang.model.element.RecordComponentElement;
-
-public record Field(String typeName, String fieldName, ParamKind paramKind, RecordComponentElement unused) {
-    public boolean isPrimitive() {
-        return ParamKind.of(unused).equals(ParamKind.PRIMITIVE);
-    }
+public record Field(String typeName, String fieldName, ParamKind paramKind) {
 }
