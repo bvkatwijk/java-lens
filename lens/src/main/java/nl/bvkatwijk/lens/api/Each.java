c1cf32d08f8db39104c1660009e75ad289914b1b
Boris van Katwijk <bvkatwijk@gmail.com>
2025-11-04 09:28:35 +0100

    Update Each.java

--- lens/src/main/java/nl/bvkatwijk/lens/api/Each.java
+++ lens/src/main/java/nl/bvkatwijk/lens/api/Each.java
@@ -5,11 +5,12 @@ import io.vavr.Value;
 import java.util.function.UnaryOperator;
 
 public class Each {
-    ///Create a function `V<T> -> V<T>` given a function `T -> T`
-    /// @param f Function `T -> T`
-    /// @return Function `V<T> -> V<T>`
+    /// Create a function `V<T> -> V<T>` given a function `T -> T`
+    ///
+    /// @param f   Function `T -> T`
     /// @param <V> Value Type
     /// @param <T> Wrapped Type
+    /// @return Function `V<T> -> V<T>`
     @SuppressWarnings("unchecked")
     public static <T, V extends Value<T>> UnaryOperator<V> mapEach(UnaryOperator<T> f) {
         return it -> (V) it.map(f);
